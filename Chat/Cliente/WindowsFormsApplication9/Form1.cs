a173f68d67cad4239669d09a2ce5df1d6724257e
Santi No Concession <santinoconcession@gmail.com>
2011-10-10 18:06:44 +0200

    lo gordo

---
+++ Chat/Cliente/WindowsFormsApplication9/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading;
+
+
+namespace WindowsFormsApplication9
+{
+    public partial class Chat : Form{
+
+        String hostname = "";
+        String version = "0.05";
+        String nombre;
+        System.Net.Sockets.TcpClient clientSocket = new System.Net.Sockets.TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readData = null;
+
+        public Chat()
+        {
+            InitializeComponent();
+            button1.Text = "Enviar";
+            button2.Text = "Conectar al servidor";
+            button3.Text = "Confirmar datos";
+            label1.Text = "Nombre";
+            label2.Text = "IP";
+            this.Text = "GregoryChat " + version;
+            this.button1.Enabled = false;
+            this.button2.Enabled = false;
+            this.textBox2.Enabled = false;
+        }
+
+        private void getMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                serverStream = clientSocket.GetStream();
+                int buffSize = 0;
+                byte[] inStream = new byte[10025];
+                buffSize = clientSocket.ReceiveBufferSize;
+                serverStream.Read(inStream, 0, buffSize);
+                string returndata = System.Text.Encoding.ASCII.GetString(inStream);
+                readData = "" + returndata;
+                msg();
+                }
+                catch (Exception e)
+                {
+                }
+            }
+        }
+
+        private void msg()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new MethodInvoker(msg));
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + Environment.NewLine + " >> " + readData;
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            escribir(textBox2.Text);
+            this.textBox2.Text = "";
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (nombre == null || hostname == null)
+            {
+                MessageBox.Show("Debes introducir un nombre");
+            }
+            else
+            {
+                readData = "Conectado";
+                msg();
+                clientSocket.Connect(hostname, 8888);
+                serverStream = clientSocket.GetStream();
+                escribir(nombre);
+                Thread ctThread = new Thread(getMessage);
+                ctThread.Start();
+                this.textBox2.Enabled=true;
+                this.button1.Enabled=true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text != "" && textBox4.Text != "")
+            {
+                nombre = textBox3.Text;
+                hostname = textBox4.Text;
+                this.button2.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Debes escribir tu nombre y la IP del servidor");
+            }
+        }
+
+        private void escribir (string linea){
+            byte[] outStream = System.Text.Encoding.ASCII.GetBytes(linea + "$");
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Flush();
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+            }
+        }
+
+        private void cierre(object sender, FormClosingEventArgs e)
+        {
+            escribir("/desconectar");
+        }
+
+    }
+}
---
+++ Chat/Servidor/ConsoleApplication1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+using System.Collections;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static Hashtable clientsList = new Hashtable();
+
+        static void Main(string[] args)
+        {
+            TcpListener serverSocket = new TcpListener(8888);
+            TcpClient clientSocket = default(TcpClient);
+            int counter = 0;
+            serverSocket.Start();
+            Console.WriteLine("Servidor iniciado...");
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+                NetworkStream networkStream = clientSocket.GetStream();
+                networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                clientsList.Add(dataFromClient, clientSocket);
+                broadcast(dataFromClient, dataFromClient, false);
+                Console.WriteLine(dataFromClient + " Se ha conectado");
+                handleClient client = new handleClient();
+                client.startClient(clientSocket, dataFromClient, clientsList);
+            }
+        }
+
+        public static void broadcast(string msg, string uName, bool flag)
+        {
+            foreach (DictionaryEntry Item in clientsList)
+            {
+                TcpClient broadcastSocket;
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+                if (Encoding.ASCII.GetBytes(msg).Equals("/desconectar")){
+                    clientsList.Remove(uName);
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " se ha desconectado");
+                }
+                else if (flag)
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " dice: " + msg);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                }
+
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
+            }
+        }
+
+
+        public class handleClient
+        {
+            TcpClient clientSocket;
+            string clNo;
+            Hashtable clientsList;
+
+            public void startClient(TcpClient inClientSocket, string clineNo, Hashtable cList)
+            {
+                this.clientSocket = inClientSocket;
+                this.clNo = clineNo;
+                this.clientsList = cList;
+                Thread ctThread = new Thread(doChat);
+                ctThread.Start();
+            }
+
+
+            private void doChat()
+            {
+                int requestCount = 0;
+                byte[] bytesFrom = new byte[10025];
+                string dataFromClient = null;
+                Byte[] sendBytes = null;
+                string serverResponse = null;
+                string rCount = null;
+                requestCount = 0;
+
+                while (true)
+                {
+                    try
+                    {
+                        requestCount = requestCount + 1;
+                        NetworkStream networkStream = clientSocket.GetStream();
+                        networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                        dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
+                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                        Console.WriteLine("From client - " + clNo + " : " + dataFromClient);
+                        rCount = Convert.ToString(requestCount);
+                        Program.broadcast(dataFromClient, clNo, true);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }                    
+                }
+            }
+        }
+    }
+}
+
